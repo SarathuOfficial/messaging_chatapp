69fe661372d75f87b4d3d4b65f1f59b5a6b90635
Sarath Kumar P M <sarath1503kumargmail.com>
2024-02-25 15:24:15 +0530

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:997502323807:web:b812be7a9eeee257af1fea";
+  static String apiKey = "AIzaSyDpSC-_GoWZro6iPb0QtNBxN7GWDGRhRWU";
+  static String messagingSenderId = "997502323807";
+  static String projectId = "chatappflutter-ad0e0";
+  final primaryColor = const Color(0xFF703EFE);
+}
